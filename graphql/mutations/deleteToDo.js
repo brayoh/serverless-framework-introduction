632595a00622edde039d15b2f73e3bdc6504a9f1
Brian Njenga <bkuriakb@gmail.com>
2020-03-06 09:04:46 +0300

    Add mutation to delete a todo item

---
+++ graphql/mutations/deleteToDo.js
@@ -0,0 +1,14 @@
+import gql from "graphql-tag";
+
+export default gql`
+  mutation DeleteToDo($id: ID!) {
+    deleteTodo(id: $id) {
+      id
+      createdAt
+      description
+      updatedAt
+      dueDate
+      completed
+    }
+  }
+`;
